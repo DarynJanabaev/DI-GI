3e7b179100ad01c4344573ea1c5c7a4604faafcd
Daryn <daryn.janabaev.daryn@mail.ru>
2022-06-05 23:04:29 +0600

    section head ready

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $(function() {
 		dots: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		nextArrow:
+			'<button type="button" class="slick-arrow slick-next"><img src="images/back.svg" alt="prev"></button>',
+		prevArrow:
+			'<button type="button" class="slick-arrow slick-prev"><img src="images/forward.svg" alt="next"></button>',
 		responsive: [
 			{
 				breakpoint: 1024,
